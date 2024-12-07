7f0c3f16a113162c814d77650a3b72caa17484ae
FarrelAdelio <farreladelioasm@gmail.com>
2024-12-07 22:11:35 +0700

    mobile friendly

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     extend: {
       fontFamily: {
         "poppins": ["Poppins"]
-      }
+      },
+      fontsize: {
+        '10xl': '120px'
+      },
+      width: {
+        '500': '500px'
+      },
+      height: {
+        '500': '500px'
+      },
+      height: {
+        '400': '400px'
+      },
     },
   },
   plugins: [],
